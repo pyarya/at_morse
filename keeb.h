2833d0b539dd93d05063c010ccb078dd7711560a
pyarya <aryan.aryal86@gmail.com>
2024-11-24 01:05:34 -0700

    circular buffer size define

--- keeb.h
+++ keeb.h
@@ -7,9 +7,11 @@
 #define DATA_PIN PD4 // data pin 
 #define CLOCK_PIN PD2 // clock pin INT0
 
+// ring buffer for handling the stream of input data
+#define KEYBOARD_BUFFER_SIZE 16 
 
-void keeb_init(void);
-char keeb_read(void);
-uint8_t keeb_available(void);
+void keeb_init(void); // initialize keyboard ports and interrupts for system
+char keeb_read(void); // reading characters from buffer
+uint8_t keeb_available(void); // checking character availability
 
 #endif
